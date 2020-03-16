d5a7a654234e5d299f6a64e0ab5d857384eeff73
Jan Lewandowski <lewandowski.jan93@gmail.com>
2020-03-16 21:02:49 +0100

    generate planets data (wrapper)

---
+++ app/src/app/components/planets-data/planets-data.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-planets-data',
+  templateUrl: './planets-data.component.html',
+  styleUrls: ['./planets-data.component.scss']
+})
+export class PlanetsDataComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
