0f2a5a07fdee010d4daf78efbf7f0979e11a7273
Jan Lewandowski <lewandowski.jan93@gmail.com>
2020-03-16 21:07:18 +0100

    add all components / services to module

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { PlanetsComponent } from './components/planets/planets.component';
+import { PlanetsDataComponent } from './components/planets-data/planets-data.component';
+import { PlanetSearchComponent } from './components/planet-search/planet-search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlanetsComponent,
+    PlanetsDataComponent,
+    PlanetSearchComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CommonModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
