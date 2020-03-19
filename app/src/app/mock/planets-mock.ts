d24cd92c9fd043a17d949a5fb3eb20ec259a8159
Jan Lewandowski <lewandowski.jan93@gmail.com>
2020-03-19 20:10:38 +0100

    Add mock data

---
+++ app/src/app/mock/planets-mock.ts
@@ -0,0 +1,109 @@
+import { PlanetData } from './../model/planet-data';
+
+export const mockPlanetData: PlanetData = {
+  count: '61',
+  next: '?page=2',
+  previous: null,
+  results: [
+    {
+      name: 'planet-1',
+      population: '10',
+      terrain: 'mountain'
+    },
+    {
+      name: 'planet-2',
+      population: '20',
+      terrain: 'forrest'
+    },
+    {
+      name: 'planet-3',
+      population: '30',
+      terrain: 'ocean'
+    },
+    {
+      name: 'planet-4',
+      population: '40',
+      terrain: 'mountain'
+    },
+    {
+      name: 'planet-5',
+      population: '50',
+      terrain: 'forrest'
+    },
+    {
+      name: 'planet-6',
+      population: '60',
+      terrain: 'ocean'
+    },
+    {
+      name: 'planet-7',
+      population: '70',
+      terrain: 'mountain'
+    },
+    {
+      name: 'planet-8',
+      population: '80',
+      terrain: 'forrest'
+    },
+    {
+      name: 'planet-9',
+      population: '90',
+      terrain: 'ocean'
+    },
+    {
+      name: 'planet-10',
+      population: '100',
+      terrain: 'mountain'
+    },
+    {
+      name: 'planet-11',
+      population: '110',
+      terrain: 'forrest'
+    },
+    {
+      name: 'planet-12',
+      population: '120',
+      terrain: 'ocean'
+    },
+    {
+      name: 'planet-13',
+      population: '130',
+      terrain: 'mountain'
+    },
+    {
+      name: 'planet-14',
+      population: '140',
+      terrain: 'forrest'
+    },
+    {
+      name: 'planet-15',
+      population: '150',
+      terrain: 'ocean'
+    },
+    {
+      name: 'planet-16',
+      population: '160',
+      terrain: 'mountain'
+    },
+    {
+      name: 'planet-17',
+      population: '170',
+      terrain: 'forrest'
+    },
+    {
+      name: 'planet-18',
+      population: '180',
+      terrain: 'ocean'
+    },
+    {
+      name: 'planet-19',
+      population: '190',
+      terrain: 'mountain'
+    },
+    {
+      name: 'planet-20',
+      population: '200',
+      terrain: 'forrest'
+    },
+  ]
+}
