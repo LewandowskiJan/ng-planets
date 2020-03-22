154542f34db806039535382cb8a5fdaa7c62027d
Jan Lewandowski <lewandowski.jan93@gmail.com>
2020-03-22 22:38:53 +0100

    fixed routes

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -1,8 +1,21 @@
+import { PlanetData } from 'src/app/model/planet-data';
+import { PlanetsComponent } from './components/planets/planets.component';
+import { PlanetDetailsComponent } from './components/planet-details/planet-details.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PlanetsDataComponent } from './components/planets-data/planets-data.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/planets/',
+    pathMatch: 'full'
+  },
+  { path: 'planet/:id', component: PlanetDetailsComponent },
+  { path: 'planets/', component: PlanetsDataComponent },
+  { path: '**', component: PlanetsDataComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
