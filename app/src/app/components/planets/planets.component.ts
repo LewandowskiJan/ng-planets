cc9716e7789ad4db267b452f495e8de292d478ba
Jan Lewandowski <lewandowski.jan93@gmail.com>
2020-03-22 22:47:04 +0100

    planets component add functionality (paging -> depends of number of items and items on page, separated api data and mock data [some functionalities work only at mock data], fixed search option, add function which is taking index of planet from url)

--- app/src/app/components/planets/planets.component.ts
+++ app/src/app/components/planets/planets.component.ts
@@ -1,8 +1,8 @@
-import { MockApiService } from './../../services/mock-api.service';
 import { PlanetDataService } from './../../services/planet-data.service';
 import { Component, OnInit } from '@angular/core';
 import { Planet } from 'src/app/model/planet';
 import { PlanetData } from 'src/app/model/planet-data';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-planets',
@@ -11,34 +11,109 @@ import { PlanetData } from 'src/app/model/planet-data';
 })
 export class PlanetsComponent implements OnInit {
 
-  constructor(
-    private planetService: PlanetDataService,
-    private mockApiService: MockApiService
-  ) { }
   data: PlanetData;
+  showedData: PlanetData;
+  pagingData: any[] = [];
+  currentPage = 1;
+  planetsOnPage = 10;
+
+  constructor(
+    private router: Router,
+    public planetService: PlanetDataService
+  ) { }
 
   ngOnInit() {
 
-    this.data = this.mockApiService.getMockData();
-    // this.planetService.getPlanetData();
-    // this.planetService.planetData.subscribe(planets => {
-    //   this.data = planets;
-    // });
-    // this.planetService.filteredPlanetData.subscribe(filteredPlanetData => {
-    //   if (filteredPlanetData.results !== []) {
-    //     this.data = filteredPlanetData;
-    //   }
-    // });
+    if (this.planetService.isUseApi) {
+      this.planetService.getPlanetDataFromApi();
+      this.planetService.planetData.subscribe(res => {
+        this.data = res;
 
+        this.pagingData.push(this.getPages(+this.data.count, this.planetsOnPage));
+        this.showedData = Object.assign({}, this.data);
+        this.showedData.results = this.data.results.slice(0, this.planetsOnPage);
 
+        this.planetService.planetData.subscribe(planets => {
+          this.data = planets;
+          this.showedData = Object.assign({}, this.data);
+          this.pagingData.push(this.getPages(+this.data.count, 10));
+        });
+      });
+    } else {
+      this.data = this.planetService.getPlanetDataFromMock();
+
+      this.planetService.planetData.next(this.data);
+      this.pagingData.push(this.getPages(+this.data.count, this.planetsOnPage));
+      this.showedData = Object.assign({}, this.data);
+      this.showedData.results = this.data.results.slice(0, this.planetsOnPage);
+
+      this.planetService.planetData.subscribe(planets => {
+        this.data = planets;
+        this.showedData = Object.assign({}, this.data);
+        this.pagingData.push(this.getPages(+this.data.count, 10));
+      });
+    }
+
+    this.planetService.filteredPlanetData.subscribe(filteredPlanetData => {
+      if (filteredPlanetData.results !== []) {
+        this.showedData = filteredPlanetData;
+      }
+    });
   }
 
-  changePage(url) {
-    this.planetService.getPlanetDataByUrl(url);
+  changeEnv() {
+    this.planetService.isUseApi = !this.planetService.isUseApi;
+  }
+
+  changePage(pageNr) {
+    this.currentPage = pageNr;
+    this.planetService.getPlanetDataByUrl(pageNr);
   }
 
   changeToPage(pageNumber) {
-    this.planetService.getPlanetByPage(pageNumber);
+    this.currentPage = pageNumber;
+    // this.planetService.getPlanetByPage(pageNumber);
+
+    // this.showedData.results = this.data.results.slice(pageNumber, this.planetsOnPage);
+    this.showedData.results = this.data.results
+      .filter((x, y) => (y < pageNumber * this.planetsOnPage))
+      .filter((x, y) => y >= pageNumber * this.planetsOnPage - this.planetsOnPage);
+  }
+
+  getDetails(url) {
+    this.router.navigate(url);
+  }
+
+  getPages(numberOfItems, itemsOnPage) {
+    let pages = 0;
+    if (numberOfItems % itemsOnPage === 0) {
+      pages = Math.floor((numberOfItems / itemsOnPage));
+    } else {
+      pages = Math.floor((numberOfItems / itemsOnPage)) + 1;
+    }
+    const tmp = [];
+    for (let i = 1; i <= pages; i++) {
+      tmp.push(i);
+    }
+    return tmp;
+  }
+
+  changeQuantityOfPlanetOnPage(qtyOfPlanetOnPage, pageNumber = 1) {
+    if (pageNumber !== 1) {
+      pageNumber = pageNumber * qtyOfPlanetOnPage;
+    } else {
+      pageNumber = pageNumber - 1;
+    }
+    this.planetsOnPage = qtyOfPlanetOnPage;
+    this.showedData.results = this.data.results.slice(pageNumber, qtyOfPlanetOnPage);
+    this.pagingData = [];
+    this.pagingData.push(this.getPages(+this.data.count, qtyOfPlanetOnPage));
+  }
+
+
+  getIdFromUrl(url) {
+    const index = url.substring(29, url.length - 1);
+    return index;
   }
 
 }
